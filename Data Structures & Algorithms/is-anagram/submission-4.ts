a425b5f6b28475377410859cd338acf9f3a7f6d3
David Muir <105323893+davidmuir1999@users.noreply.github.com>
2026-05-14 16:30:33 +0100

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.ts
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+
+        if (s.length != t.length) return false;
+
+        const count: Record<string, number> = {};
+
+        for(const c of s){
+            count[c] = (count[c] ?? 0) + 1;
+        }
+
+        for(const c of t){
+            if (!count[c]) return false;
+            count[c]--
+        }
+        return true;
+    }
+}
