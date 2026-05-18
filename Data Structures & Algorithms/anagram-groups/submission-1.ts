9a50f8725d02b82d8a4a3c2a1ff5f94e7953151c
David Muir <105323893+davidmuir1999@users.noreply.github.com>
2026-05-18 23:17:43 +0100

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs: string[]): string[][] {
+        const map = new Map();
+
+        for(let str of strs){
+            const sortedStr = str.split('').sort().join('');
+
+            if(!map.has(sortedStr)){
+                map.set(sortedStr, []);
+            }
+
+            map.get(sortedStr).push(str);
+        }
+
+        return Array.from(map.values());
+    }
+}
