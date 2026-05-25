11b041217faa54a65ffe0f90985f931bba2d6427
David Muir <105323893+davidmuir1999@users.noreply.github.com>
2026-05-25 14:55:36 +0100

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public List<List<string>> GroupAnagrams(string[] strs) {
+        var dict = new Dictionary<string, List<string>>();
+
+        foreach(var str in strs){
+            var chars = str.ToCharArray();
+            Array.Sort(chars);
+
+            var key = new string(chars);
+
+            if(!dict.ContainsKey(key)){
+                dict[key] = new List<string>();
+            }
+
+            dict[key].Add(str);
+        }
+        return dict.Values.ToList();
+    }
+}
