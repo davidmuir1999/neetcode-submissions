350bc4f50c1fdff213dbc5866ddeb07c66fc2211
David Muir <105323893+davidmuir1999@users.noreply.github.com>
2026-06-02 14:20:56 +0100

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.ts
@@ -0,0 +1,31 @@
+class Solution {
+    /**
+     * @param {character[][]} board
+     * @return {boolean}
+     */
+    isValidSudoku(board: string[][]): boolean {
+        const seen = new Set();
+
+        for(let r = 0; r < 9; r++){
+            for(let c = 0; c < 9; c++){
+                const val  = board[r][c]
+
+                if (val === '.') continue;
+
+                const rowKey = `row-${[r]}-${val}`;
+                const colKey = `col-${[c]}-${val}`;
+                const boxKey = `box-${Math.floor(r/3)}-${Math.floor(c/3)}-${val}`;
+
+                if (seen.has(rowKey) || seen.has(colKey) || seen.has(boxKey)){
+                    return false;
+                }
+
+                seen.add(rowKey);
+                seen.add(colKey);
+                seen.add(boxKey);
+            }
+        }
+
+        return true;
+    }
+}
