3260bd9b815baac2428bcae994222d815156009d
David Muir <105323893+davidmuir1999@users.noreply.github.com>
2026-06-07 16:08:31 +0100

    Add: evaluate-reverse-polish-notation - submission-8

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-8.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int EvalRPN(string[] tokens) {
+        var stack = new Stack<int>();
+
+        foreach (string token in tokens) {
+            if (token == "+" || token == "-" || token == "*" || token == "/") {
+                int b = stack.Pop();
+                int a = stack.Pop();
+
+                if (token == "+") {
+                    stack.Push(a + b);
+                } else if (token == "-") {
+                    stack.Push(a - b);
+                } else if (token == "*") {
+                    stack.Push(a * b);
+                } else if (token == "/") {
+                    stack.Push(a / b);
+                }
+            } else {
+                stack.Push(int.Parse(token));
+            }
+        }
+        return stack.Pop();
+    }
+}
