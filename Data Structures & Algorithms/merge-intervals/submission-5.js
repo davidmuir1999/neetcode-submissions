958051ca7a7a2da71dca623e5c4f328081897f14
David Muir <105323893+davidmuir1999@users.noreply.github.com>
2026-06-26 16:50:42 +0100

    Add: merge-intervals - submission-5

---
+++ Data Structures & Algorithms/merge-intervals/submission-5.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals) {
+        if(intervals.length === 0) return [];
+
+        intervals.sort((a,b) => a[0] - b[0]);
+
+        const res = [intervals[0]];
+
+        for(let i = 1; i < intervals.length; i++){
+
+            const lastMerged = res[res.length - 1];
+            const current = intervals[i];
+
+            if(lastMerged[1] >= current[0]){
+                lastMerged[1] = Math.max(lastMerged[1], current[1])
+            } else {
+                res.push(current)
+            }
+        }
+        return res;
+    }
+}
