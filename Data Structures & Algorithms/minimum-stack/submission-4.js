d10d02609fe3bbdde6f0fce1ca34f147c5fd4a4d
David Muir <105323893+davidmuir1999@users.noreply.github.com>
2026-06-28 13:57:00 +0100

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.js
@@ -0,0 +1,48 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop();
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length -1];
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        const temp = [];
+
+        let mini = this.stack[this.stack.length - 1];
+
+        while(this.stack.length > 0){
+            mini = Math.min(mini, this.stack[this.stack.length - 1]);
+
+            temp.push(this.stack.pop())
+        }
+
+        while(temp.length > 0){
+            this.stack.push(temp.pop())
+        }
+
+        return mini;
+    }
+}
