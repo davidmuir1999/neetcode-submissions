02f1085871100b385dad767e959c5e5d9f488588
David Muir <105323893+davidmuir1999@users.noreply.github.com>
2026-06-29 10:34:25 +0100

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.js
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+     * @param {number} target
+     * @param {number[]} position
+     * @param {number[]} speed
+     * @return {number}
+     */
+    carFleet(target, position, speed) {
+        const cars = []
+
+        for(let i = 0; i < position.length; i++){
+            cars.push({pos:position[i], speed: speed[i]});
+        }
+
+        cars.sort((a,b) => b.pos - a.pos);
+
+        const stack = [];
+
+        for(let car of cars){
+            const time = (target - car.pos)/car.speed;
+
+            if(stack.length === 0 || time > stack[stack.length - 1]){
+                stack.push(time);
+            }
+        }
+
+        return stack.length;
+    }
+}
